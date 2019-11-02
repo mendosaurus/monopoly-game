0ce9a8f519adaf70a4af3cd12a21904bfbf78a43
mendosaurus <masaki.endo@nex.nikkei.com>
2019-11-02 11:09:15 -0400

    refactor code by class Player

--- index.js
+++ index.js
@@ -1,60 +1,40 @@
-// square array
+// Example of a VERY simple Monopoly game simulation
 const squares = [100, -10, 0, 0, -40, -10, -10, 5, 0, -10, -50, -10, 0, 0, -50, -10]
 
-let player1 = {
-  name: 'Joaquim',
-  color: 'red',
-  position: 0,
-  cash: 1000,
-  move: function() {
-    let dice = 1+Math.floor(6*Math.random());
-    this.position = (this.position + dice) % squares.length;
-    this.cash += squares[this.position];
-    if (this.cash < 0) {
-      console.log(`Game over for ${this.name}.`);
-    }
-  },
-  displayInfo: function() {
-    console.log(`${this.name} is at position ${this.position} and has ${this.cash}€`);
+// refactoring previous code by class Player
+class Player {
+  // `constructor` is the method triggered with the `new` keyword 
+  constructor(name,color){
+    this.name = name;
+    this.color = color;
+    this.position = 0;
+    this.cash = 1000;
   }
+
+  // move method
+  move() {
+      let dice = 1 + Math.floor(6 * Math.random());
+      this.position = (this.position + dice) % squares.length;
+      this.cash += squares[this.position];
+      if (this.cash < 0) {
+        console.log(`Game over for ${this.name}.`);
+      }
+    }
+  
+    // displayInfo method
+  displayInfo() {
+      console.log(`${this.name} is at position ${this.position} and has ${this.cash}€`);
+    }
 }
 
-let player2 = {
-  name: 'Maxence',
-  color: 'blue',
-  position: 0,
-  cash: 1000,
-  move: function() {
-    let dice = 1+Math.floor(6*Math.random());
-    this.position = (this.position + dice) % squares.length;
-    this.cash += squares[this.position];
-    if (this.cash < 0) {
-      console.log(`Game over for ${this.name}.`);
-    }
-  },
-  displayInfo: function() {
-    console.log(`${this.name} is at position ${this.position} and has ${this.cash}€`);
-  }
-}
 
-let player3 = {
-  name: 'Mostafa',
-  color: 'black',
-  position: 0,
-  cash: 1000,
-  move: function() {
-    let dice = 1+Math.floor(6*Math.random());
-    this.position = (this.position + dice) % squares.length;
-    this.cash += squares[this.position];
-    if (this.cash < 0) {
-      console.log(`Game over for ${this.name}.`);
-    }
-  },
-  displayInfo: function() {
-    console.log(`${this.name} is at position ${this.position} and has ${this.cash}€`);
-  }
-}
+// initialize each players using Player class
+let player1 = new Player('Joaquim', 'red')
+let player2 = new Player('Maxence', 'blue')
+let player3 = new Player('Mostafa', 'black')
 
+
+// update the game
 function upDate(){
   // --- Turn  ---
   player1.move();
